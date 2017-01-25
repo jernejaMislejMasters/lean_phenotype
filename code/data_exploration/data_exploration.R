b3e186c1f0e2458a3cb01530c053cb9ec2a3cfa8
Jerneja Mislej <jerneja.mislej@gmail.com>
2017-01-25 14:53:27 +0100

    data exploration, variables with description, type and summary

--- code/data_exploration/data_exploration.R
+++ code/data_exploration/data_exploration.R
@@ -2,7 +2,60 @@
 
 
 VIP_data <- read.csv("VIP_161102.csv", header = TRUE, sep = ",", row.names = NULL, fill=TRUE)
+VIP_data_copy<-VIP_data
 
-#for subjects with at least two visits, check the time difference between the first two visits and get the ennumer for the first two visits
+#VIP_data_working_size=VIP_data[1:1000,]
 
-VIP_data_working_size=VIP_data[1:1000,]
+for (variable in c(4,6:length(colnames(VIP_data)))) {
+
+	if (length(levels(factor(VIP_data_copy[,variable])))<20) {
+		missing_values=length(VIP_data[is.na(VIP_data[,variable]),1])
+		#message(paste(colnames(VIP_data)[variable],":"))
+		variable_value_counts<-aggregate(VIP_data[,1]~VIP_data[,variable], FUN=length)
+		for (variable_class in variable_value_counts[,1]){
+
+			if (variable_class=='5555' || variable_class=='6666' || variable_class=='7777' || variable_class=='8888' || variable_class=='9999' || variable_class==''){
+				missing_values=missing_values+variable_value_counts[2][variable_value_counts[1]==variable_class]
+			}else{
+
+				cat(paste(variable_class,":",variable_value_counts[2][variable_value_counts[1]==variable_class],", "))
+			}
+			
+		}
+		message(paste("missing values :",missing_values))
+	}else{
+
+		message("\n")
+	}
+}
+
+
+for (variable in c(4,6:length(colnames(VIP_data)))) {
+
+	if (length(levels(factor(VIP_data_copy[,variable])))>5 && colnames(VIP_data)[variable]!='ursprungsland_vilket' && colnames(VIP_data)[variable]!='enkver') {
+		missing_values=length(VIP_data[is.na(VIP_data[,variable]),1])
+		message(paste(colnames(VIP_data)[variable],":"))
+		variable_values=na.omit(VIP_data[,variable])
+		variable_values=variable_values[variable_values!='8888']
+		variable_values=variable_values[variable_values!='9999']
+		variable_values=variable_values[variable_values!='6666']
+		variable_values=variable_values[variable_values!='5555']
+		variable_values=variable_values[variable_values!='7777']
+		variable_value_counts<-aggregate(VIP_data[,1]~VIP_data[,variable], FUN=length)
+		for (variable_class in variable_value_counts[,1]){
+
+			if (variable_class=='5555' || variable_class=='6666' || variable_class=='7777' || variable_class=='8888' || variable_class=='9999' || variable_class==''){
+				missing_values=missing_values+variable_value_counts[2][variable_value_counts[1]==variable_class]
+			}
+			
+		}
+		cat(paste("mean :",round(mean(variable_values),2),", min :",round(min(variable_values),2),", max :",round(max(variable_values),2),", sd :",round(sd(variable_values),2)))
+		message(paste(", missing values :",missing_values))
+	}else{
+
+		message("\n")
+	}
+}
+
+
+
